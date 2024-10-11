529073bbf30f4f83a668dfffd5eb2a66983e107d
予蒙 爨 <3075252381@qq.com>
2024-10-11 22:20:30 +0800

    删除update批注

--- Obstacle.cs
+++ Obstacle.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour
 {
     public float speed;
-    
+   
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
